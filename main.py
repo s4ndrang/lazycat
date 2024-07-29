724e757392c6c6450f03d02f5990fa1f8bbbbdb7
ngs <ngs@uqtr.ca>
2024-07-29 02:38:14 -0400

    improved functionality

--- main.py
+++ main.py
@@ -24,104 +24,144 @@ text_font = pygame.font.Font('img/GloriousChristmas-BLWWB.ttf', 40)
 
 sky_surface = pygame.image.load('img/bigsky.jpeg')
 text_surface = text_font.render('Le Chat Paresseux', False, 'Black')
-winthegame_surface = text_font.render('YOU WON !!!', True, 'Black')
+chooseRows_surface = text_font.render('How many blocks per row (3-10)? ', False, 'Black')
+winthegame_surface = text_font.render('YOU WON !!! click anywhere to start a new game', False, 'Black')
 kitty_surface = pygame.image.load('img/kittenTrans50.png')
 cat_surface = pygame.image.load('img/cat50.png')
 ground_surface = pygame.image.load('img/ground50.jpeg')
 brick_surface = pygame.image.load('img/brick50.jpeg')
 
-stop = False
-while stop == False:
-    nbRangee = input("Combien de bloques par rangée (3-10)? ")
-    nbRangee = int(nbRangee)
-    if nbRangee > 2 and nbRangee < 11:
-        stop = True
+def display_background(nbRangee):
+    screen.blit(sky_surface, (0, 0))
+    text_rect = text_surface.get_rect(center = (700, (335-(50*nbRangee + 5*nbRangee-1)/2)/2 + 10))
+    # text_rect = text_surface.get_rect(center = (700, 85))
+    screen.blit(text_surface, text_rect)
 
-        #debut position briques
-        brick_rect = brick_surface.get_rect(topleft = (700-(50*nbRangee + 5*nbRangee-1)/2, 335-(50*nbRangee + 5*nbRangee-1)/2))
+while True:
+    
+    #block image transfer (put one surface on another)
+    display_background(3)
 
-        #debut position titre
-        text_rect = text_surface.get_rect(center = (700, (335-(50*nbRangee + 5*nbRangee-1)/2)/2 + 10))
+    #game question
+    chooseRows_rect = chooseRows_surface.get_rect(center = (width/2, height/2))
 
-        #position winning text
-        winthegame_rect = text_surface.get_rect(topleft = (900, 500))
+    #position winning text
+    winthegame_rect = winthegame_surface.get_rect(center = (700, 575))
+    
+    stop = False
+    while stop == False:
+        screen.blit(chooseRows_surface, chooseRows_rect)
+        nbRangee = 0
 
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                print(event.unicode)
+                nbRangee = event.unicode
 
-        #pour positionner chat
-        randint1 = random.randrange(0, nbRangee)
-        randint2 = random.randrange(0, nbRangee)
-        cat_rect = cat_surface.get_rect(topleft = (700-(50*nbRangee + 5*nbRangee-1)/2 + 55*randint1, 335-(50*nbRangee + 5*nbRangee-1)/2 + 55*randint1))
-        print("cat_rect.x ", cat_rect.x)
-        print("cat_rect.y ", cat_rect.y)
+        nbRangee = int(nbRangee)
 
-        listofx = []
-        listofy = []
-        posx = 100
-        posy = 100
-
-        for y in range(nbRangee):
-            for x in range(nbRangee):
-                if brick_rect.x not in listofx:
-                    listofx.append(brick_rect.x)
-                brick_rect.x += 55
-            brick_rect.x = 700-(50*nbRangee + 5*nbRangee-1)/2
-            listofy.append(brick_rect.y)
-            brick_rect.y += 55
-
-        print("listofx ", listofx)
-        print("listofy ", listofy)
-
-        while True:
+        if nbRangee > 2 and nbRangee < 11:
             
-            #block image transfer (put one surface on another)
-            screen.blit(sky_surface, (0, 0))
-            screen.blit(text_surface, text_rect)
-            screen.blit(cat_surface, cat_rect)
+            brick_rect = brick_surface.get_rect(topleft = (700-(50*nbRangee + 5*nbRangee-1)/2, 335-(50*nbRangee + 5*nbRangee-1)/2))
+        
 
-            brick_rect.x = 700-(50*nbRangee + 5*nbRangee-1)/2
-            brick_rect.y = 335-(50*nbRangee + 5*nbRangee-1)/2
+            #pour positionner chat
+            randint1 = random.randrange(0, nbRangee)
+            randint2 = random.randrange(0, nbRangee)
+            cat_rect = cat_surface.get_rect(topleft = (700-(50*nbRangee + 5*nbRangee-1)/2 + 55*randint1, 335-(50*nbRangee + 5*nbRangee-1)/2 + 55*randint2))
+
+            #put the x position of the top left corner of each brick in an array
+            listofx = []
+            listofy = []
+            posx = 100
+            posy = 100
 
             for y in range(nbRangee):
                 for x in range(nbRangee):
-                    if x != posx or y != posy:
-                        screen.blit(brick_surface, (brick_rect.x, brick_rect.y))
+                    if brick_rect.x not in listofx:
+                        listofx.append(brick_rect.x)
                     brick_rect.x += 55
                 brick_rect.x = 700-(50*nbRangee + 5*nbRangee-1)/2
+                listofy.append(brick_rect.y)
                 brick_rect.y += 55
 
-        # list of events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+            stop = True
 
-        # when player clicks on a square, reveal what is under
-                if event.type == pygame.MOUSEBUTTONUP:
-                    mousex, mousey = pygame.mouse.get_pos()
-                    posx = len(listofx)-1
-                    posy = len(listofy)-1
-                    
-                    for x in range(len(listofx)-1):
-                        if mousex >= listofx[x] and mousex < listofx[x+1]:
-                            posx = x
-                            break
+            while stop:
+                #debut position briques
+                display_background(nbRangee)
+
+                #debut position titre
+                # text_rect = text_surface.get_rect(center = (700, (335-(50*nbRangee + 5*nbRangee-1)/2)/2 + 10))
+
+                screen.blit(cat_surface, cat_rect)
+
+                brick_rect.x = 700-(50*nbRangee + 5*nbRangee-1)/2
+                brick_rect.y = 335-(50*nbRangee + 5*nbRangee-1)/2
+
+                for y in range(nbRangee):
+                    for x in range(nbRangee):
+                        if x != posx or y != posy:
+                            screen.blit(brick_surface, (brick_rect.x, brick_rect.y))
+                        brick_rect.x += 55
+                    brick_rect.x = 700-(50*nbRangee + 5*nbRangee-1)/2
+                    brick_rect.y += 55
+
+                if posx == listofx.index(cat_rect.x) and posy == listofy.index(cat_rect.y):
+                    nbRangee = 0
+                    won = True
+                    while won:
                         
-                    for y in range(len(listofy)-1):
-                        if mousey >= listofy[y] and mousey < listofy[y+1]:
-                            posy = y
-                            break
+                        screen.blit(winthegame_surface, winthegame_rect)
 
-                    if posx == listofx.index(cat_rect.x) and posy == listofy.index(cat_rect.y):
-                        nbRangee = 0
-                        stop = False
-                        print("YOU WON")
-                        break
-                        # while stop == False:
-                        #     screen.blit(winthegame_surface, winthegame_rect)
-                        #     pygame.display.update()
-                        #     clock.tick(60)
+                        for event in pygame.event.get():
+                            #if player clicks 'ESC', quit the game doesnt work
+                            if event.type == pygame.QUIT: # if event.key == pygame.K_ESCAPE: doesnt work
+                                pygame.quit()
+                                exit()
 
-            pygame.display.update()
+                            # if player clicks anyway, start new game
+                            if event.type == pygame.MOUSEBUTTONUP:
+                                display_background(nbRangee)
+                                stop = False
+                                won = False
 
-            #max 60 frames/sec
-            clock.tick(60)
+                        pygame.display.update()
+                        clock.tick(60)
+
+            # list of events
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+
+            # when player clicks on a square, reveal what is under
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        mousex, mousey = pygame.mouse.get_pos()
+                        posx = len(listofx)-1
+                        posy = len(listofy)-1
+                        
+                        for x in range(len(listofx)-1):
+                            if mousex >= listofx[x] and mousex < listofx[x+1]:
+                                posx = x
+                                break
+                            
+                        for y in range(len(listofy)-1):
+                            if mousey >= listofy[y] and mousey < listofy[y+1]:
+                                posy = y
+                                break
+
+                pygame.display.update()
+
+                #max 60 frames/sec
+                clock.tick(60)
+
+        pygame.display.update()
+
+        #max 60 frames/sec
+        clock.tick(60)
+
+    pygame.display.update()
+
+    #max 60 frames/sec
+    clock.tick(60)
